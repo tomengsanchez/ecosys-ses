10631d44c1ec44c4ff79949eac2d6f90326e1d04
tomengsanchez <mrtomy31@gmail.com>
2021-10-30 09:48:08 +0800

    Update ecosys-ses.php

--- ecosys-ses.php
+++ ecosys-ses.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Ecosys SES Monitoring
  * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handle the basics with this plugin.
+ * Description:       Ecosys SES monitoring system
  * Version:           1.10.3
  * Requires at least: 5.2
  * Requires PHP:      7.2
@@ -15,6 +15,28 @@
  * Domain Path:       /languages
  */
 
+// add_menupage
+add_action('admin_menu',function(){
+    add_menu_page('Ecosys SES','Ecosys Socio Economic Survey','manage_options','ecosys-ses','ses_main_function','');
+    add_submenu_page('ecosys-ses','Global Search','Global Search','manage_options','global-search','ecosys_ses_global_search');
 
- 
+    add_menu_page('Ecosys SCM','Ecosys Stakeholder Consultation Meetings','manage_options','ecosys-scm','scm_main_function','');
+    add_submenu_page('ecosys-scm','Add Projects','Add Projects','manage_options','scm-add-projects','ecosys_scm_add_projects');
+    
+});
+function ses_main_function(){
+    echo "Hi";
+}
+
+function scm_main_function(){
+    echo "Hi";
+}
+
+function ecosys_ses_global_search(){
+    echo "12";
+}
+
+function ecosys_scm_add_projects(){
+    echo "fuck";
+}
  ?>
