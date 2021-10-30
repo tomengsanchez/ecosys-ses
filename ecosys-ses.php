8bcc8441dcaa0a911a5fd2b8494434fda1857d08
tomengsanchez <mrtomy31@gmail.com>
2021-10-30 18:20:54 +0800

    Set up Boostrap

--- ecosys-ses.php
+++ ecosys-ses.php
@@ -11,32 +11,54 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Text Domain:       ecosys-ses
  * Domain Path:       /languages
+ * 
  */
 
+
 // add_menupage
+
+// Enqueue
+$valid_pages = array('ecosys-ses','ecosys-scm');
+if(!empty($_GET['page'])){
+    if(in_array($_GET['page'],$valid_pages)){
+
+        add_action('admin_enqueue_scripts',function(){
+            wp_enqueue_style('ecosys-ses-bs-css', plugin_dir_url(__FILE__) . '/css/bootstrap.css');
+            wp_enqueue_script('ecosys-ses-bs-js', plugin_dir_url(__FILE__) . '/js/bootstrap.js');
+            
+        });
+    }
+}
+
+
+
 add_action('admin_menu',function(){
     add_menu_page('Ecosys SES','Ecosys SES','manage_options','ecosys-ses','ses_main_function','');
     add_submenu_page('ecosys-ses','Global Search','Global Search','manage_options','global-search','ecosys_ses_global_search');
 
     add_menu_page('Ecosys SCM','Ecosys SCM','manage_options','ecosys-scm','scm_main_function','');
     add_submenu_page('ecosys-scm','Add Projects','Add Projects','manage_options','scm-add-projects','ecosys_scm_add_projects');
-    
+        
 });
+
 function ses_main_function(){
-    echo "Hi";
+    ?>
+        <ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
+    <?php
 }
-
 function scm_main_function(){
-    echo "Hi";
+    echo "1212";
 }
-
 function ecosys_ses_global_search(){
-    echo "12";
+    echo "Global Search";
 }
-
 function ecosys_scm_add_projects(){
-    echo "fuck";
+    echo "Add Projects";
 }
  ?>
